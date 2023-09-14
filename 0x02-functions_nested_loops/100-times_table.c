e4694b633d4d40601ef4f718ee6df916018771cd
Charlestimi <charlestimiebifemere@gmail.com>
2023-09-14 04:43:35 -0700

    timestable100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,9 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the n times table, starting with 0
- *
- * @n: number of the times table
- */
+* print_times_table - prints the n times table, starting with 0
+* @n: number of the times table
+*/
 void print_times_table(int n)
 {
 int i, j, k;
@@ -40,7 +40,7 @@ _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0')
+_putchar((k % 10) + '0');
 }
 }
 _putchar('\n');
